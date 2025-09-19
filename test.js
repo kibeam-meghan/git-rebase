c979a43e4e1a9f13cd24553b77988660cfcbebe3
Dominic Aquilina <aquilina.dominic@gmail.com>
2025-09-19 16:31:46 -0400

    Added subtractNumbers

--- test.js
+++ test.js
@@ -5,3 +5,11 @@ function addNumbers(a, b) {
 
 // Should output 3
 console.log(addNumbers(1, 2));
+
+function subtractNumbers(a, b) {
+
+  return a - b;
+}
+
+// Should output -1
+console.log(subtractNumbers(1, 2));
